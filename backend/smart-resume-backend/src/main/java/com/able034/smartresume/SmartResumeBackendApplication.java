2b7ab89cbcfa4ae027aa571d8934e4346fbb736c
Able034 <carry041105@qq.com>
2026-06-23 23:21:14 +0800

    Initialize Spring Boot backend

---
+++ backend/smart-resume-backend/src/main/java/com/able034/smartresume/SmartResumeBackendApplication.java
@@ -0,0 +1,14 @@
+package com.able034.smartresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartResumeBackendApplication.class, args);
+		System.out.println("(♥◠‿◠)ﾉﾞ  smart-resume-backend启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+	}
+
+}
---
+++ backend/smart-resume-backend/src/test/java/com/able034/smartresume/SmartResumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.able034.smartresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartResumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
